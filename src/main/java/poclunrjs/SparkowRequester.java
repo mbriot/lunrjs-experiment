7eb10c5cfec72d6f17e1e6c074694eb2953a23e0
Manuel Briot <manuel.briot@decathlon.com>
2017-01-04 11:15:04 +0100

    manage proxy

--- src/main/java/poclunrjs/SparkowRequester.java
+++ src/main/java/poclunrjs/SparkowRequester.java
@@ -1,39 +1,49 @@
 package poclunrjs;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
-import org.apache.commons.httpclient.methods.GetMethod;
+import java.io.IOException;
+import java.nio.charset.Charset;
 
-import java.io.*;
-import java.util.zip.GZIPInputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class SparkowRequester {
 
     private String sparkowHost;
+    
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+    RequestConfig config;
+    
+    
+    //ENABLED IN OFFICE ONLY
+    {
+    	CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(
+                new AuthScope("gateway.zscaler.net", 80),
+                new UsernamePasswordCredentials("GIHQ4ALL", "GIHQ4ALL"));
+    	httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+    	HttpHost proxy = new HttpHost("gateway.zscaler.net", 80, "http");
+        config = RequestConfig.custom().setProxy(proxy).build();
+    }
 
-    private static MultiThreadedHttpConnectionManager connectionManager =
-            new MultiThreadedHttpConnectionManager();
-    private static HttpClient client = new HttpClient(connectionManager);
-
-    public String requestSparkow(String path) throws IOException {
-        GetMethod method = new GetMethod("http://" + sparkowHost + ".net/json/" + path);
-        method.setRequestHeader(new Header("Accept-Encoding", "gzip"));
-        byte[] responseBody;
-        String response = null;
-        client.executeMethod(method);
-        responseBody = method.getResponseBody();
-
-        GZIPInputStream ungzippedResponse = new GZIPInputStream(new ByteArrayInputStream(responseBody));
-        Reader reader = new InputStreamReader(ungzippedResponse, "UTF-8");
-        Writer writer = new StringWriter();
-        char[] buffer = new char[10240];
-        for (int length = 0; (length = reader.read(buffer)) > 0; ) {
-            writer.write(buffer, 0, length);
-        }
-        response = writer.toString();
-        method.releaseConnection();
-        return response;
+    public String requestSparkow(String path)   throws IOException  {
+    	String result = "";
+    	HttpGet httpGet = new HttpGet("http://" + sparkowHost + ".net/json/" + path);
+    	httpGet.setConfig(config);
+        httpGet.addHeader("Accept-Encoding", "gzip");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+        result = EntityUtils.toString(entity, Charset.forName("UTF-8").name());  
+        return result;
     }
 
     public SparkowRequester(String sparkowHost) {
